cbfc77da98d2df736582f6cfe91e1f207c0c43a3
adebayo oluwafemi <femi.adebayo1998@gmail.com>
2020-01-09 17:29:45 +0100

    new changes to tags

--- src/components/businesscards.jsx
+++ src/components/businesscards.jsx
@@ -20,21 +20,21 @@ class BusinessCard extends Component {
         </div>
         <h1 className="text-center mt-5 md:text-4xl" style={{ color: '#01b0f0' }}><b>Choose Product</b></h1>
         <div className="md:flex mx-20">
-          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '43rem' }}>
+          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '46rem' }}>
             <img src={Onesided} alt='onesided' className=":w-11\/12" />
             <h1 className="text-center md:text-2xl mt-2 text-lg">One-Sided Business Cards</h1>
-            <p className="text-center text-sm">Order one sided business cards that simply<br />
+            <p className="text-center text-sm mt-1">Order one sided business cards that simply<br />
              represent your business in special ways. Order<br /> with our free templates, upload your own designs<br />
              or have a team design your business cards at a <br />friendly fee.
             </p>
-            <div className="my-2 mx-4 text-center"><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options.
+            <div className="my-2 mx-4 text-center">
+              <h1><b>Material</b>- 300 gsm matte card paper stock with 600 gsm options.</h1>
             <br />
-            <b>Finishing</b> - Matte Lamination with round corner options
+            <h1><b>Finishing</b> - Matte Lamination with round corner options</h1>
             <br/>
-              <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities
+             <h1> <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities</h1>
            </div>
-            <p className="font-bold text-center">
-              Starting at <span className="line-through font-bold">₦5,500.00</span>,
+            <p className="font-bold text-center">Starting at <span className="line-through font-bold">₦5,500.00</span>,
            <br/>
               <span style={{ color: '#ec268f' }}>₦3,500.00</span>
             </p>
@@ -43,23 +43,24 @@ class BusinessCard extends Component {
                 <span>Order Now</span>
               </button>
             </div>
-            <span className="text-center " style={{ marginLeft: '7rem' }}>View Prices</span>
+            <a href="/" className="text-center " style={{ marginLeft: '7rem' }}>View Prices</a>
           </div>
-          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '43rem' }}>
+          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '46rem' }}>
             <img src={Twosided} alt='onesided' className=":w-11\/12" />
             <h1 className="text-center md:text-2xl mt-2">Two-sided Business Cards</h1>
-            <p className="text-center text-sm">Order premium double-sided business cards and<br />
+            <p className="text-center text-sm mt-1">Order premium double-sided business cards and<br />
               start presenting your business in awesome ways.<br />
               Customise one of our templates, upload your own<br /> pre-designed files or have our in-house designers<br />
               create something awesome for you.</p>
-            <div className="my-2 mx-4 text-center"><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options
+            <div className="my-2 mx-4 text-center">
+              <h1><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options</h1>
             <br/>
-              <b>Finishing</b> - Matte Lamination with round corner options
+            <h1> <b>Finishing</b> - Matte Lamination with round corner options</h1>
             <br/>
-              <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities
+            <h1> <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities</h1>
            </div>
             <div className="font-bold text-center">
-              Starting at <span className="line-through font-bold">₦6,999.00</span>
+             <h1> Starting at <span className="line-through font-bold">₦6,999.00</span></h1>
               <br />
               <span style={{ color: '#ec268f' }}>₦4,999.00</span>
             </div>
@@ -68,21 +69,25 @@ class BusinessCard extends Component {
                 <span>Order Now</span>
               </button>
             </div>
-            <span className="text-center " style={{ marginLeft: '7rem' }}>View Prices</span>
+            <a href="/" className="text-center " style={{ marginLeft: '7rem' }}>View Prices</a>
           </div>
-          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '43rem' }}>
+          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '48rem' }}>
             <img src={threesided} alt='onesided' className=":w-11\/12" />
             <h1 className="text-center md:text-2xl mt-2">Square Business Cards (1 sided)</h1>
-            <p className="text-center text-sm">Square it up, because everyone is going<br /> rectangular. Create unique square-shaped <br />business cards and wow everyone. Choose from<br /> our free designs, upload your own or have our<br /> design team help you fix something beautiful.
+            <p className="text-center text-sm mt-1">Square it up, because everyone is going<br/>
+             rectangular. Create unique square-shaped <br />business cards and wow everyone. Choose from<br/>
+              our free designs, upload your own or have our<br/> 
+              design team help you fix something beautiful.
             </p>
-            <div className="my-2 mx-4 text-center"><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options
+            <div className="my-2 mx-4 text-center">
+              <h1><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options</h1>
             <br/>
-              <b>Finishing</b> - Matte Lamination with round corner options
+            <h1><b>Finishing</b> - Matte Lamination with round corner options</h1>
             <br/>
-              <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities
+            <h1><b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities</h1>
             </div>
             <div className="font-bold text-center">
-              Starting at <span className="line-through font-bold">₦5,000.00.</span>
+            <h1> Starting at <span className="line-through font-bold">₦5,000.00.</span></h1>
               <br />
               <span style={{ color: '#ec268f' }}>₦3,000.00</span>
             </div>
@@ -91,21 +96,27 @@ class BusinessCard extends Component {
                 <span>Order Now</span>
               </button>
             </div>
-            <span className="text-center " style={{ marginLeft: '7rem' }}>View Prices</span>
+            <a href="/" className="text-center " style={{ marginLeft: '7rem' }}>View Prices</a>
           </div>
         </div>
         <div className="md:flex mx-20">
-          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '45rem' }}>
+          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '51rem' }}>
             <img src={Fouthsided} alt='onesided' className=":w-11\/12" />
             <h1 className="text-center md:text-2xl mt-2">Square Business Cards (2 sided)</h1>
-            <p className="text-center text-sm">Be different, create full colour square business <br />cards with rounded corner options. Choose and<br /> customize one of our 1000S of free templates or<br /> upload your own designs. You can also have a<br /> design team come up with something <br />awesome for you.</p>
-            <div className="my-2 mx-4 text-center"><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options
+            <p className="text-center text-sm mt-1">Be different, create full colour square business <br/>
+            cards with rounded corner options. Choose and<br/> 
+            customize one of our 1000S of free templates or<br/> 
+            upload your own designs. You can also have a<br/> 
+            design team come up with something <br />awesome for you.</p>
+            <div className="my-2 mx-4 text-center">
+              <h1><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options</h1>
            <br/>
-              <b>Finishing</b> - Matte Lamination with round corner options
+           <h1><b>Finishing</b> - Matte Lamination with round corner options</h1>
            <br/>
-              <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities</div>
+           <h1><b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities</h1>
+              </div>
             <div className="font-bold text-center">
-              Starting at <span className="line-through font-bold">₦6,999.00</span>,
+              <h1>Starting at <span className="line-through font-bold">₦6,999.00</span></h1>
            <br/>
               <span style={{ color: '#ec268f' }}>₦4,999.00</span>
             </div>
@@ -114,64 +125,94 @@ class BusinessCard extends Component {
                 <span>Order Now</span>
               </button>
             </div>
-            <span className="text-center " style={{ marginLeft: '7rem' }}>View Prices</span>
+            <a href="/" className="text-center " style={{ marginLeft: '7rem' }}>View Prices</a>
           </div>
-          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '43rem' }}>
+          <div className="border mt-10 mb-2 mr-20" style={{ width: '20rem', height: '47rem' }}>
             <img src={fifthsided} alt='onesided' className=":w-11\/12" />
             <h1 className="text-center md:text-2xl mt-2">Mini Business Card</h1>
-            <p className="text-center text-sm">Order full colour quality mini business cards that<br /> simply represent your business in special ways.<br /> Order with our free templates, upload your own<br /> designs or have a team design your business cards<br /> at a friendly fee.
+            <p className="text-center text-sm mt-1">Order full colour quality mini business cards that<br /> 
+            simply represent your business in special ways.<br/> 
+            Order with our free templates, upload your own<br/> 
+            designs or have a team design your business cards<br/> 
+            at a friendly fee.
             </p>
-            <div className="my-2 mx-4 text-center"><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options
+            <div className="my-2 mx-4 text-center">
+              <h1><b>Material</b> - 300 gsm matte card paper stock with 600 gsm options.</h1>
             <br/>
-              <b>Finishing</b> - Matte Lamination with round corner options
+            <h1> <b>Finishing</b> - Matte Lamination with round corner options</h1>
            <br/>
-              <b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities
+           <h1><b>Delivery</b> - 3 - 5 working days for Lagos, 5 - 7 working days for other cities.</h1>
            </div>
             <div className="font-bold text-center">
-              Starting at <span className="line-through font-bold">₦5,000.00</span>,
+            <h1>  Starting at <span className="line-through font-bold">₦5,000.00</span>,</h1>
             <br/>
-              <span style={{ color: '#ec268f' }}>₦3,000.00</span></div>
+              <span style={{ color: '#ec268f' }}>₦3,000.00</span>
+              </div>
             <div className="flex justify-center mt-3">
               <button className="border w-3/5 h-10 rounded mb-5" style={{ backgroundColor: '#ec268f', color: 'white' }}>
                 <span>Order Now</span>
               </button>
             </div>
-            <span className="text-center " style={{ marginLeft: '7rem' }}>View Prices</span>
+            < a href="/" className="text-center " style={{ marginLeft: '7rem' }}>View Prices</a>
           </div>
         </div>
         <div className=" mx-20  md:border  mt-10   rounded-lg mb-5 pb-5 h-auto" style={{ backgroundColor: '#dae1e7' }}>
-          <div className="md:text-2xl text-center pt-5">What customers say.</div>
+          <div className="md:text-2xl text-center pt-5">
+            <h1>What customers say.</h1>
+          </div>
           <div className="md:flex">
             <div className="md:flex mr-5 ml-10 mt-10 mb-5">
               <div className="">
                 <img src={Customer1} alt='image' classname=":w-4\/12 " style={{ height: '30%', borderRadius: '5rem' }} />
               </div>
-              <div className="">All our business cards  <span style={{ color: '#01b0f0' }}><b>@Hotelng</b></span> are<br /> made by <span style={{ color: '#01b0f0' }}><b>@printivo</b>.</span> Really no easier<br /> way to print stuff. <span style={{ color: '#01b0f0' }}><b>@MarkEssien</b>
+              <div className="">
+                <p>All our business cards  <span style={{ color: '#01b0f0' }}><b>@Hotelng</b></span> are<br/>
+                 made by <span style={{ color: '#01b0f0' }}><b>@printivo</b>.</span> Really no easier<br/> 
+                 way to print stuff. <span style={{ color: '#01b0f0' }}><b>@MarkEssien</b>
               </span>
+              </p>
               </div>
             </div>
             <div className="md:flex  ml-10 mr-10 mt-10 mb-5">
               <div>
                 <img src={Customer1} alt='image' classname=":w-4\/12 " style={{ height: '30%', borderRadius: '5rem' }} />
               </div>
-              <div className="mr-10"><span style={{ color: '#01b0f0' }}><b>@Printivo</b></span> got an order delivered in 2<br /> days!!!! Talk about excellence! Lovely<br /> to see Nigerian businesses deliver on <br />their brand promise. <span style={{ color: '#01b0f0' }}><b>@MonsieurUwem</b></span></div>
+              <div className="mr-10">
+                <p><span style={{ color: '#01b0f0' }}><b>@Printivo</b></span> got an order delivered in 2<br/> 
+                days!!!! Talk about excellence! Lovely
+              <br/> to see Nigerian businesses deliver on 
+              <br/>their brand promise. <span style={{ color: '#01b0f0' }}><b>@MonsieurUwem</b></span>
+              </p>
+              </div>
             </div>
             <div className="md:flex mr-10 ml-10  mt-10 mb-5">
               <div className="">
                 <img src={Customer3} alt='image' classname=":w-4\/12 " style={{ height: '30%', borderRadius: '5rem' }} />
               </div>
-              <div className=""><span style={{ color: '#01b0f0' }}><b>@Printivo</b></span> delivered just in time.<br /> Thank you so much for the great<br /> service <span style={{ color: '#01b0f0' }}><b>@abbybells</b></span></div>
+              <div className="">
+                <p>
+                <span style={{ color: '#01b0f0' }}>
+                <b>@Printivo</b></span> delivered just in time.<br/> Thank you so much for the great<br/>
+                 service <span style={{ color: '#01b0f0' }}><b>@abbybells</b></span>
+              </p>
+              </div>
             </div>
           </div>
         </div>
         <div className="mt-20">
-          <div className="md:text-2xl text-center" style={{ color: '#01b0f0' }}><b>Tips and ideas from our blog</b></div>
+          <div className="md:text-2xl text-center" style={{ color: '#01b0f0' }}>
+            <h1><b>Tips and ideas from our blog</b></h1>
+            </div>
         </div>
         <div className="md:flex ml-20 my-10 ">
           <div className="mr-5 mb-10">
             <img src={Tip1} alt='tip1' />
-            <h1 className="md:text-1xl mt-2"><b>Seven Ways to Make <br />Your Business Card Your Best Marketing Tool</b></h1>
-            <p className="mt-2">Every day we hand out a tiny brochure about our <br />businesses without even realizing it. Look at the stacks<br /> on your desk, the deck in your drawer or the stash in<br /> your wallet. Business cards, in all sizes, shapes and <br />colors, are everywhere. And yet, while we ll understand... <br /><a href="" style={{ color: '#ec268f' }}>Read More</a></p>
+            <h1 className="md:text-1xl mt-2"><b>Seven Ways to Make <br/>Your Business Card Your Best Marketing Tool</b></h1>
+            <p className="mt-2">Every day we hand out a tiny brochure about our <br/>
+            businesses without even realizing it. Look at the stacks<br/>
+            on your desk, the deck in your drawer or the stash in<br /> your wallet. Business cards, in all sizes, shapes and <br/>
+            colors, are everywhere. And yet, while we ll understand... <br /><a href="" style={{ color: '#ec268f' }}>Read More</a>
+            </p>
           </div>
           <div className="mr-8 mb-10">
             <img src={Tip2} alt='tip1' />
